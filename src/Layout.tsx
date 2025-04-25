07f6dcae4557195f6c6049d7b090ccc650009748
miriamsobrino <miriamsobrinodev@gmail.com>
2025-04-25 17:07:50 +0200

    change background image

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,13 @@ const Layout = () => {
       <Header />
       <img
         src='./bg-emoji.jpg'
-        className='w-full absolute inset-0 -z-2 opacity-60 mask-radial-from-1.5 h-screen  object-bottom-center object-cover '
+        style={{
+          maskImage:
+            'radial-gradient(circle, rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
+          WebkitMaskImage:
+            'radial-gradient(circle,  rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%)',
+        }}
+        className='w-full absolute inset-0 -z-2 opacity-60  h-screen  object-bottom-center object-cover '
       />
       <div className='bg-white w-full h-screen absolute inset-0 -z-1 opacity-10'></div>
       <main className='w-full z-50 bg-cover bg-center bg-no-repeat  bg-opacity-50 h-screen flex flex-col justify-center items-center'>
